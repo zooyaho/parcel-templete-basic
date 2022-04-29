62f34c7dae98f54e742e8b84f59d0151907aca03
zooyaho <jiwoo1268@naver.com>
2022-04-29 13:57:46 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+// import autoprefixer from 'autoprefixer'와 같은 의미
+
+/* 
+export {
+  plugins: [
+    autoprefixer
+  ]
+} 와 같은 의미 
+*/
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
+
